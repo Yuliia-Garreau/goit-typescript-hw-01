5a99cd02b6e190f4e16d50a3e0e6c3978dc94fc5
Yuliia Garreu <yuliia.garreau@gmail.com>
2025-03-08 21:54:14 +0000

    corrected

--- src/generics/2.ts
+++ src/generics/2.ts
@@ -5,14 +5,25 @@ type AllType = {
   weight: number;
 };
 
-function compare(
-  top: AllType,
-  bottom: AllType
-): Pick<AllType, "name" | "position" | "color" | "weight"> {
+// function compare(
+//   top: AllType,
+//   bottom: AllType
+// ): Pick<AllType, "name" | "position" | "color" | "weight"> {
+//   return {
+//     name: top.name,
+//     color: top.color,
+//     position: bottom.position,
+//     weight: bottom.weight,
+//   };
+// }
+function compare<
+  TopPick extends Pick<AllType, keyof AllType>,
+  BottomPick extends Pick<AllType, keyof AllType>
+>(top: TopPick, bottom: BottomPick): AllType {
   return {
     name: top.name,
     color: top.color,
     position: bottom.position,
     weight: bottom.weight,
-  };
+  } as AllType;
 }
